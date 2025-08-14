3db97809e4d2975572250e4bcaf71d14729054ec
AntonZaiets <133760646+AntonZaiets@users.noreply.github.com>
2025-08-15 00:14:59 +0300

    refactor: update TaskList types, optimize filtering with `useCallback`, and fix minor type checks

--- frontend/src/components/TaskList/TaskList.tsx
+++ frontend/src/components/TaskList/TaskList.tsx
@@ -1,7 +1,9 @@
-import { type Task, useStore } from "@/store/useTaskStore";
-import { useDeleteTodo } from "@/hooks/useDeleteTodo";
-import { useToggleTodo } from "@/hooks/useToggleTodo";
-import { useTodos } from "@/hooks/useTodos";
+import { useStore } from '@/store/useTaskStore';
+import { useDeleteTodo } from '@/hooks/useDeleteTodo';
+import { useToggleTodo } from '@/hooks/useToggleTodo';
+import { useTodos } from '@/hooks/useTodos';
+import { TTask } from '../../../../shared/types';
+import { useCallback } from 'react';
 
 export const TaskList = () => {
   const { data: tasks, isLoading } = useTodos();
@@ -9,22 +11,26 @@ export const TaskList = () => {
   const toggleTask = useToggleTodo();
   const filterValue = useStore((store) => store.filter);
 
-  const shouldDisplayTask = (task: Task) => {
-    if (filterValue === "all") return true;
-    if (filterValue === "completed" && task.completed) return true;
-    return filterValue === "incomplete" && !task.completed;
-  };
+  const shouldDisplayTask = useCallback(
+    (task: TTask) => {
+      if (filterValue === 'all') return true;
+      if (filterValue === 'completed' && task.completed) return true;
+      return filterValue === 'incomplete' && !task.completed;
+    },
+    [filterValue],
+  );
+  console.log(tasks);
 
   if (isLoading) return <p>Loading...</p>;
-  if (Object.keys(tasks).length === 0) return <p>No todos yet</p>;
+  if (tasks?.length === 0) return <p>No todos yet</p>;
 
   return (
     <div>
       <h1>Task List</h1>
       <ul>
         {tasks
-          ?.filter((task: Task) => shouldDisplayTask(task))
-          .map((task: Task) => (
+          ?.filter((task: TTask) => shouldDisplayTask(task))
+          .map((task: TTask) => (
             <li key={task.id}>
               {task.title}
               {task.completed ? <p>Done</p> : <p>In progress</p>}
